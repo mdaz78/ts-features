7c113eead4c3c17d6481fd9e13c8c8878c9c4421
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-29 17:26:05 +0530

    feat: add different types of function syntax

--- src/annotations/functions.ts
+++ src/annotations/functions.ts
@@ -2,5 +2,30 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
-const result = add(1, 2);
-console.log(result);
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// call every function
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(divide(1, 2));
+console.log(multiply(1, 2));
+console.log(logger('Hello'));
+console.log(throwError('Error'));
