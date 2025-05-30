257748df1763b3bf968a6220a311782b101abbda
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-30 12:15:41 +0530

    adding functions to interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,21 +1,21 @@
-// Anytime we create an interface we are creating a new type
-// Start with a capital letter
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: true;
+  summary(): string;
 }
 
 const oldCivic: Vehicle = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
