a980a3766956c86d362bcb364fbbe037297af6d9
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-30 11:35:08 +0530

    typing 2d arrays

--- src/arrays.ts
+++ src/arrays.ts
@@ -3,3 +3,6 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 
 // Explicitly typed array
 const stringArray: string[] = [];
+
+// 2D Arrays
+const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
