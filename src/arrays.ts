87df3bf36a6dc3ad75682e1c0579697f027f7e83
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-30 11:38:01 +0530

    learn why typed arrays are important

--- src/arrays.ts
+++ src/arrays.ts
@@ -6,3 +6,14 @@ const stringArray: string[] = [];
 
 // 2D Arrays
 const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+// help with inference when extracting values
+const car = carMakers[0];
+
+// prevent incompatible values
+// carMakers.push(100);
+
+// help with map
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
